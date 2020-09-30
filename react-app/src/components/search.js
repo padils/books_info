9fdb6e993a34ba5c706b7816cafe96c2d97191f6
padils <bobo-00@inbox.ru>
2020-09-30 20:43:37 +0300

    search

--- react-app/src/components/search.js
+++ react-app/src/components/search.js
@@ -7,6 +7,7 @@ const Search =()=>{
 
     let {addFilter}=useContext(BooksContext)
 
+
     return <div class="input-field ">
           <i class="material-icons prefix ">search</i>
           <input  onChange={e=>addFilter(e.target.value)} type="text" className="validate "/>
